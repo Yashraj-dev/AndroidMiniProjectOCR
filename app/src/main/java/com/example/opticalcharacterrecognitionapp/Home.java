7ce484fb5e0605fab93952547cd13b92483ea0aa
Yashraj575 <117842328+Yashraj575@users.noreply.github.com>
2023-07-10 09:23:17 +0530

    Completed project Phase 2

--- app/src/main/java/com/example/opticalcharacterrecognitionapp/Home.java
+++ app/src/main/java/com/example/opticalcharacterrecognitionapp/Home.java
@@ -3,13 +3,10 @@ package com.example.opticalcharacterrecognitionapp;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.net.Uri;
@@ -32,13 +29,15 @@ import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Home extends AppCompatActivity {
     private Button captureBtn, historyBtn;
     private Bitmap bitmap;
-    private static final int REQUEST_CAMERA_CODE = 100;
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int REQUEST_IMAGE_SELECTION = 2;
     String username;
 
     @Override
@@ -50,14 +49,14 @@ public class Home extends AppCompatActivity {
         try {
             Bundle bundle = getIntent().getBundleExtra("data");
             username = bundle.getString("Username");
-        }catch (Exception e){
+        } catch (Exception e) {
             Log.e("myTag", "" + e);
         }
 
         captureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                captureImage();
+                openImageSelection();
             }
         });
 
@@ -73,14 +72,19 @@ public class Home extends AppCompatActivity {
         });
     }
 
-    private void captureImage() {
-        CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).start(Home.this);
+    private void openImageSelection() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_SELECTION);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+        if (requestCode == REQUEST_IMAGE_SELECTION && resultCode == RESULT_OK && data != null) {
+            Uri selectedImageUri = data.getData();
+            loadSelectedImage(selectedImageUri);
+        } else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
                 Uri resultUri = result.getUri();
@@ -95,10 +99,56 @@ public class Home extends AppCompatActivity {
                 Exception error = result.getError();
                 Toast.makeText(Home.this, "Crop error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        } else {
-            Log.i("resultCode", String.valueOf(resultCode));
         }
     }
+
+    private void loadSelectedImage(Uri imageUri) {
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri), null, options);
+
+            // Calculate the desired image width and height
+            int maxWidth = 1024;
+            int maxHeight = 1024;
+            int imageWidth = options.outWidth;
+            int imageHeight = options.outHeight;
+            int scaleFactor = Math.min(imageWidth / maxWidth, imageHeight / maxHeight);
+
+            // Decode the image file with the calculated sample size
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = scaleFactor;
+            Bitmap selectedImage = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri), null, options);
+
+            // Start the crop activity with the scaled-down image
+            startCropActivity(selectedImage);
+        } catch (IOException e) {
+            Toast.makeText(Home.this, "Error loading selected image", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    private void startCropActivity(Bitmap bitmap) {
+        try {
+            // Create a temporary file to save the bitmap
+            File tempFile = File.createTempFile("crop_image", ".jpg", getCacheDir());
+            FileOutputStream out = new FileOutputStream(tempFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.close();
+
+            // Get the Uri of the temporary file
+            Uri tempUri = Uri.fromFile(tempFile);
+
+            // Start the crop activity with the temporary file's Uri
+            CropImage.activity(tempUri)
+                    .setGuidelines(CropImageView.Guidelines.ON)
+                    .start(this);
+        } catch (IOException e) {
+            Toast.makeText(Home.this, "Error loading selected image", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
 
     private void detectText(Bitmap bitmap) {
         InputImage image = InputImage.fromBitmap(bitmap, 0);
@@ -141,8 +191,8 @@ public class Home extends AppCompatActivity {
                     myIntent.putExtra("capturedImage", bitmap);
                     startActivity(myIntent);
                 } catch (Exception e) {
-                    Log.e("myTag", "" + e );
-//                    Toast.makeText(Home.this, "Failed to detect text from Image", Toast.LENGTH_SHORT).show();
+                    Log.e("myTag", "" + e);
+                    Toast.makeText(Home.this, "", Toast.LENGTH_SHORT).show();
                 }
             }
         }).addOnFailureListener(new OnFailureListener() {
